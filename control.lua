459a8d971c516842b19b2f0e1a2ed96abd4ddb73
Wiwiweb <wiwiweb2@gmail.com>
2023-03-10 20:50:36 -0800

    Just detect Nullius ourselves
    It's the main usecase anyway

--- control.lua
+++ control.lua
@@ -4,7 +4,38 @@ require("scripts/flush")
 require("scripts/removal")
 require("scripts/destruction")
 require("scripts/remote-interface")
-require("scripts/commands")
+
+local function check_for_nullius()
+  if script.active_mods["nullius"] then
+    local undeletable_fluids = {
+      "nullius-sludge",
+      "nullius-chlorine",
+      "nullius-hydrogen-chloride",
+      "nullius-acid-hydrochloric",
+      "nullius-ethylene",
+      "nullius-propene",
+      "nullius-benzene",
+      "nullius-acid-sulfuric",
+      "nullius-butadiene",
+      "nullius-styrene",
+      "nullius-acrylonitrile",
+      "nullius-ech",
+      "nullius-glycerol",
+      "nullius-lubricant",
+      "nullius-solvent",
+      "nullius-epoxy",
+      "nullius-titanium-tetrachloride",
+      "nullius-acid-nitric",
+      "nullius-fatty-acids",
+      "nullius-oil",
+      "nullius-biodiesel",
+      "nullius-copper-solution",
+    }
+    global.undeletable_fluids = list_to_set(undeletable_fluids)
+  else
+    global.undeletable_fluids = {}
+  end
+end
 
 local function on_init()
   -- unit number (of the mined entity) -> 
@@ -13,5 +44,11 @@ local function on_init()
   global.saved_surrounding_fluids_by_unit_number = {}
   -- set of fluid names
   global.undeletable_fluids = {}
+  check_for_nullius()
 end
 script.on_init(on_init)
+
+local function on_configuration_changed()
+  check_for_nullius()
+end
+script.on_configuration_changed(on_configuration_changed)
