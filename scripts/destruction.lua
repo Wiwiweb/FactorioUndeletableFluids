63c38167f225d1063e59cbcad2ec3891a3ecaef5
Wiwiweb <wiwiweb2@gmail.com>
2023-10-16 17:57:22 -0700

    Preserve temperature on destruction prevention

--- scripts/destruction.lua
+++ scripts/destruction.lua
@@ -2,28 +2,36 @@ local function prevent_destruction(event)
   -- We can't just clone the entity and then let Factorio continue with the removal,
   -- because that will prevent existing pipes from connecting to the new entity (they are still connected to the old).
   -- We have to actively destroy it here.
-  local surface = event.entity.surface
-  local fluids = event.entity.get_fluid_contents()
-  local fluid_name = event.entity.fluidbox[1].name
+  local entity = event.entity
+  local surface = entity.surface
+
+  -- Save fluids before we destroy the entity.
+  -- We can't just use get_fluid_contents() because we need temperature.
+  local fluids = {}
+  for i = 1, #entity.fluidbox do
+    table.insert(fluids, entity.fluidbox[i])
+  end
+
   local new_entity_params = {
-    name = event.entity.name,
-    position = event.entity.position,
-    direction = event.entity.direction,
-    force = event.entity.force,
+    name = entity.name,
+    position = entity.position,
+    direction = entity.direction,
+    force = entity.force,
     create_build_effect_smoke = false,
     spawn_decorations = false,
   }
 
-  event.entity.destroy() -- Destroy the old entity *before* creating the new one to ensure pipe connections.
+  entity.destroy() -- Destroy the old entity *before* creating the new one to ensure pipe connections.
   local new_entity = surface.create_entity(new_entity_params)
 
   -- Restore fluids as they were during pre-mining
   -- For destruction, we only need to restore the destroyed entity
-  for fluid, amount in pairs(fluids) do
-    new_entity.insert_fluid({name = fluid, amount = amount})
+  for _, fluid in pairs(fluids) do
+    new_entity.insert_fluid(fluid)
   end
 
   if event.cause and event.cause.player then
+    local fluid_name = fluids[1].name
     create_error_message(event.cause.player, {"undeletable-fluids.mining_prevented"}, fluid_name, new_entity_params.position)
   end
 end
